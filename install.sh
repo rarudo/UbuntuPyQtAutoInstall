6334dc5966739a4947282caef12585677ac1c1e1
rarudo <rarudonet@gmail.com>
2017-09-20 19:16:37 +0900

    qmakeのパス指定を変更

--- install.sh
+++ install.sh
@@ -11,12 +11,17 @@ apt-get -y install build-essential
 # font ライブラリの導入
 apt-get -y install libfontconfig1
 
+# python開発者用パッケージのインストール
+apt-get -y install python3.5-dev
+
 #apt-get install -y mesa-common-dev libglu1-mesa-dev
 
 # qtのインストル
 cd /tmp
 wget http://download.qt.io/official_releases/qt/5.9/5.9.1/qt-opensource-linux-x64-5.9.1.run
-bash qt-opensource-linux-x64-5.9.1.run
+chmod +x qt-opensource-linux-x64-5.9.1.run
+./qt-opensource-linux-x64-5.9.1.run
+
 
 # sipのインストール
 wget https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.3/sip-4.19.3.tar.gz
@@ -31,12 +36,6 @@ cd /tmp
 wget https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.9/PyQt5_gpl-5.9.tar.gz
 tar zxvf PyQt5_gpl-5.9.tar.gz
 cd PyQt5_gpl-5.9
-python3 configure.py --qmake ~/Qt/5.8/clang_64/bin/qmake
+python3 configure.py --qmake ~/Qt5.9.1/5.9.1/gcc_64/bin/qmake
 make
 make install
-
-
-
-
-
-
