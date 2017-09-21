5c41ca3f19d4a6f289f09d476940023695b8e759
rarudo <rarudonet@gmail.com>
2017-09-21 11:39:00 +0900

    パスの修正

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get -y update
 apt-get -y upgrade
 
 # vm用ツールのインストール
-apt-get -y install open-vm-tools sudo apt-get install open
+apt-get -y install open-vm-tools open-vm-tools-desktop
 
 # g++の導入
 apt-get -y install build-essential
@@ -18,7 +18,7 @@ apt-get -y install python3.5-dev
 
 # qtのインストル
 cd /tmp
-wget http://download.qt.io/official_releases/qt/5.9/5.9.1/qt-opensource-linux-x64-5.9.1.run
+curl -L -O http://download.qt.io/official_releases/qt/5.9/5.9.1/qt-opensource-linux-x64-5.9.1.run
 chmod +x qt-opensource-linux-x64-5.9.1.run
 ./qt-opensource-linux-x64-5.9.1.run
 
@@ -29,13 +29,13 @@ tar zxvf sip-4.19.3.tar.gz
 cd sip-4.19.3
 python3 configure.py
 make
-make install
+sudo make install
 
 # pyqtのインストール
 cd /tmp
 wget https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.9/PyQt5_gpl-5.9.tar.gz
 tar zxvf PyQt5_gpl-5.9.tar.gz
 cd PyQt5_gpl-5.9
-python3 configure.py --qmake ~/Qt5.9.1/5.9.1/gcc_64/bin/qmake
-make
-make install
+python3 configure.py --qmake /opt/Qt5.9.1/5.9.1/gcc_64/bin/qmake
+sudo make
+sudo make install
