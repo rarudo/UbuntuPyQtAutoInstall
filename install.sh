8d7eda80a30ff8cb47feef4cdfa267bde56e851f
rarudo <rarudonet@gmail.com>
2017-09-21 20:37:01 +0900

    不要な処理を削除

--- install.sh
+++ install.sh
@@ -55,7 +55,6 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc 
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
-bash
 
 # 指定バージョンのpythonをインストール
 pyenv install $INSTALL_PYTHON_VERSION
